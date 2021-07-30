52b058a7d409f6deca83580207e02528fc5a9513
Jon Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-07-30 15:07:04 -0700

    Change datetime to datetimeoffset

--- src/ClimateChangeIndicators.Data/Entities/Entry.cs
+++ src/ClimateChangeIndicators.Data/Entities/Entry.cs
@@ -5,7 +5,7 @@ namespace ClimateChangeIndicators.Data.Entities
     public class Entry
     {
         public int Id { get; set; }
-        public DateTime Date { get; set; }
+        public DateTimeOffset Date { get; set; }
         public double MeasuredValue { get; set; }
         public int UnitOfMeasurementId { get; set; }
         public UnitOfMeasurement UnitOfMeasurement { get; set; } = null!;

8d9e64275a3471f4fc92a5e7fe04424ed535c67d
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2021-09-19 13:18:12 -0700

    Add index page for Actions

---
+++ src/ClimateChangeIndicators.App/Pages/Actions/Index.razor.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Components.Routing;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.Web.Virtualization;
+using Microsoft.JSInterop;
+using ClimateChangeIndicators.App;
+using ClimateChangeIndicators.App.Shared;
+using MudBlazor;
+using Microsoft.EntityFrameworkCore;
+using ClimateChangeIndicators.Data;
+using Action = ClimateChangeIndicators.Data.Entities.Action;
+
+namespace ClimateChangeIndicators.App.Pages.Actions
+{
+    public partial class Index
+    {
+        private bool _isLoaded;
+        private Action _selectedItem = null!;
+
+        [Inject]
+        public IDbContextFactory<AppDbContext> ContextFactory { get; set; } = null!;
+
+        [Inject]
+        public NavigationManager Navigation { get; set; } = null!;
+        public AppDbContext Context { get; private set; } = null!;
+        public List<Action> Actions { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            try {
+                Context = ContextFactory.CreateDbContext();
+                Actions = await Context.Actions
+                    .AsNoTracking()
+                    .AsSingleQuery()
+                    .ToListAsync();
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex);
+            }
+            finally {
+                _isLoaded = true;
+            }
+        }
+    }
+}

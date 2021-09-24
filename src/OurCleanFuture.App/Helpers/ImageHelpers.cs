de8381af695126545e537bad81199fc578aceba4
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2021-09-24 16:42:16 -0700

    Add null checks to ImageHelpers

--- src/OurCleanFuture.App/Helpers/ImageHelpers.cs
+++ src/OurCleanFuture.App/Helpers/ImageHelpers.cs
@@ -11,26 +11,36 @@ namespace OurCleanFuture.App.Helpers
     {
         public static string GetAreaIconPath(Area area)
         {
-            return area.Id switch {
-                1 => "/images/transportation.png",
-                2 => "/images/homes-and-buildings.png",
-                3 => "/images/energy-production.png",
-                4 => "/images/people-and-the-environment.png",
-                5 => "/images/communities.png",
-                6 => "/images/innovation.png",
-                7 => "/images/leadership.png",
-                _ => ""
-            };
+            if (area is not null) {
+                return area.Id switch {
+                    1 => "/images/transportation.png",
+                    2 => "/images/homes-and-buildings.png",
+                    3 => "/images/energy-production.png",
+                    4 => "/images/people-and-the-environment.png",
+                    5 => "/images/communities.png",
+                    6 => "/images/innovation.png",
+                    7 => "/images/leadership.png",
+                    _ => ""
+                };
+            }
+            else {
+                return string.Empty;
+            }
         }
         public static string GetGoalIconPath(Goal goal)
         {
-            return goal.Id switch {
-                1 => "/images/reduce-greenhouse-gas-emissions.png",
-                2 => "/images/ensure-yukoners-have-access-to-reliable-affordable-and-renewable-energy.png",
-                3 => "/images/adapt-to-the-impacts-of-climate-change.png",
-                4 => "/images/build-a-green-economy.png",
-                _ => ""
-            };
+            if (goal is not null) {
+                return goal.Id switch {
+                    1 => "/images/reduce-greenhouse-gas-emissions.png",
+                    2 => "/images/ensure-yukoners-have-access-to-reliable-affordable-and-renewable-energy.png",
+                    3 => "/images/adapt-to-the-impacts-of-climate-change.png",
+                    4 => "/images/build-a-green-economy.png",
+                    _ => ""
+                };
+            }
+            else {
+                return string.Empty;
+            }
         }
     }
 }

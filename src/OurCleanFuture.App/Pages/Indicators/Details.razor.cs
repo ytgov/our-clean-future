92a6e7337b2244db8057c1483bcc87c74be26e8a
Jon.Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-10-28 20:25:38 -0700

    Refactor last updated date logic on indicator details page

--- src/OurCleanFuture.App/Pages/Indicators/Details.razor.cs
+++ src/OurCleanFuture.App/Pages/Indicators/Details.razor.cs
@@ -23,17 +23,15 @@ namespace OurCleanFuture.App.Pages.Indicators
 {
     public partial class Details
     {
-        public List<ChartSeries> Series = new();
-        public ChartOptions Options = new();
-        public string[] XAxisLabels = { "2019", "2020", "2021", "2022", "2023", "2024" };
         private bool isLoaded;
         private AppDbContext context = null!;
-        private DateTime targetUpdatedDate = DateTime.MinValue;
 
         [Parameter]
         public int Id { get; set; }
 
         public Indicator Indicator { get; set; } = null!;
+
+        public DateTime IndicatorLastUpdated { get; set; }
 
         [Inject]
         public IDbContextFactory<AppDbContext> ContextFactory { get; set; } = null!;
@@ -66,22 +64,10 @@ namespace OurCleanFuture.App.Pages.Indicators
                     .ThenInclude(a => a!.Objective)
                     .ThenInclude(o => o.Goals)
                     .AsSingleQuery()
-                    .FirstOrDefaultAsync(i => i.Id == Id);
-                if (Indicator?.Target != null) {
-                    targetUpdatedDate = context.Entry(Indicator.Target).Property<DateTime>("ValidFrom").CurrentValue;
-                }
-                else {
-                    targetUpdatedDate = await context.Targets
-                        .TemporalAll()
-                        .Where(t => t.IndicatorId == Indicator!.Id)
-                        .OrderBy(t => EF.Property<DateTime>(t, "ValidTo"))
-                        .Select(t => EF.Property<DateTime>(t, "ValidTo"))
-                        .LastAsync();
-                }
+                    .FirstAsync(i => i.Id == Id);
+                IndicatorLastUpdated = await GetIndicatorLastUpdatedDate();
 
 #pragma warning restore CS8601 // Possible null reference assignment.
-                double[] Data1 = { 26, 42, 49, 72 };
-                Series.Add(new ChartSeries() { Name = $"{Indicator?.Title} ({Indicator?.UnitOfMeasurement})", Data = Data1 });
             }
             catch (Exception ex) {
                 Console.WriteLine(ex);
@@ -93,9 +79,22 @@ namespace OurCleanFuture.App.Pages.Indicators
             await base.OnInitializedAsync();
         }
 
-        private DateTime GetIndicatorLastUpdatedDate()
+        private async Task<DateTime> GetIndicatorLastUpdatedDate()
         {
-            var indicatorUpdatedDate = context.Entry(Indicator).Property<DateTime>("ValidFrom").CurrentValue;
+            var targetUpdatedDate = DateTime.MinValue;
+            if (Indicator?.Target != null) {
+                targetUpdatedDate = context.Entry(Indicator.Target).Property<DateTime>("ValidFrom").CurrentValue;
+            }
+            else {
+                targetUpdatedDate = await context.Targets
+                    .TemporalAll()
+                    .Where(t => t.IndicatorId == Indicator!.Id)
+                    .OrderBy(t => EF.Property<DateTime>(t, "ValidTo"))
+                    .Select(t => EF.Property<DateTime>(t, "ValidTo"))
+                    .LastAsync();
+            }
+
+            var indicatorUpdatedDate = context.Entry(Indicator!).Property<DateTime>("ValidFrom").CurrentValue;
             //An indicator might not have a target, in which case we return the indicatorUpdatedDate
             return indicatorUpdatedDate > targetUpdatedDate ? indicatorUpdatedDate : targetUpdatedDate;
         }

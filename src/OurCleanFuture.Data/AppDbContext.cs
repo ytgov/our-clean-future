fd97e99015469aba63849a40aa2102eb940e59f8
Jon.Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-11-24 13:36:56 -0700

    Change DbSet properties to read-only, and initialize

--- src/OurCleanFuture.Data/AppDbContext.cs
+++ src/OurCleanFuture.Data/AppDbContext.cs
@@ -6,17 +6,17 @@ namespace OurCleanFuture.Data;
 
 public class AppDbContext : DbContext
 {
-    public DbSet<Indicator> Indicators { get; set; } = null!;
-    public DbSet<Lead> Leads { get; set; } = null!;
-    public DbSet<Branch> Branches { get; set; } = null!;
-    public DbSet<Department> Departments { get; set; } = null!;
-    public DbSet<UnitOfMeasurement> UnitsOfMeasurement { get; set; } = null!;
-    public DbSet<DirectorsCommittee> DirectorsCommittees { get; set; } = null!;
-    public DbSet<Action> Actions { get; set; } = null!;
-    public DbSet<Target> Targets { get; set; } = null!;
-    public DbSet<Goal> Goals { get; set; } = null!;
-    public DbSet<Objective> Objectives { get; set; } = null!;
-    public DbSet<Area> Areas { get; set; } = null!;
+    public DbSet<Indicator> Indicators => Set<Indicator>();
+    public DbSet<Lead> Leads => Set<Lead>();
+    public DbSet<Branch> Branches => Set<Branch>();
+    public DbSet<Department> Departments => Set<Department>();
+    public DbSet<UnitOfMeasurement> UnitsOfMeasurement => Set<UnitOfMeasurement>();
+    public DbSet<DirectorsCommittee> DirectorsCommittees => Set<DirectorsCommittee>();
+    public DbSet<Action> Actions => Set<Action>();
+    public DbSet<Target> Targets => Set<Target>();
+    public DbSet<Goal> Goals => Set<Goal>();
+    public DbSet<Objective> Objectives => Set<Objective>();
+    public DbSet<Area> Areas => Set<Area>();
 
     private readonly ConnectionStrings connectionStrings;
 
@@ -128,7 +128,7 @@ public class AppDbContext : DbContext
                             ie.ToTable("Entries");
                             ie.Property<int>("IndicatorId")
                                 .HasColumnType("int");
-                            ie.HasKey("IndicatorId", "Id", "StartDate");
+                            ie.HasKey("IndicatorId", "StartDate");
                             ie.ToTable(tb => tb.IsTemporal(tb => {
                                 tb.UseHistoryTable("EntriesHistory");
                                 tb.HasPeriodStart("ValidFrom");

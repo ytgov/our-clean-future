9ab4cdd48c9c27d443465836697f3359e6d1851d
Jon.Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-11-23 19:24:40 -0700

    Change the primary key of Entries use a natural key, which will prevent duplicate periods from being entered on an indicator

--- src/OurCleanFuture.Data/Entities/Entry.cs
+++ src/OurCleanFuture.Data/Entities/Entry.cs
@@ -2,7 +2,6 @@
 
 public class Entry
 {
-    public int Id { get; set; }
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public double Value { get; set; }
@@ -10,4 +9,19 @@ public class Entry
     public string UpdatedBy { get; set; } = "";
 
     public Indicator Indicator { get; set; } = null!;
+
+    public string EntryValueToString()
+    {
+        var result = Value.ToString();
+        // Switching on Id here, as end users have access to change the Name and Symbol.
+        result = Indicator.UnitOfMeasurement.Id switch {
+            // Count
+            2 => Value.ToString(),
+            // Dollars
+            4 => Value.ToString("c"),
+            // All other units
+            _ => $"{Value} {Indicator.UnitOfMeasurement.Symbol}",
+        };
+        return result;
+    }
 }
---
+++ src/OurCleanFuture.Data/Migrations/20211124000813_UseNaturalKeyEntries.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OurCleanFuture.Data.Migrations
+{
+    public partial class UseNaturalKeyEntries : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Entries",
+                table: "Entries")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EntriesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Entries")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EntriesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Entries",
+                table: "Entries",
+                columns: new[] { "IndicatorId", "StartDate" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Entries",
+                table: "Entries")
+                .Annotation("SqlServer:IsTemporal", true)
+                .Annotation("SqlServer:TemporalHistoryTableName", "EntriesHistory")
+                .Annotation("SqlServer:TemporalHistoryTableSchema", null)
+                .Annotation("SqlServer:TemporalPeriodEndColumnName", "ValidTo")
+                .Annotation("SqlServer:TemporalPeriodStartColumnName", "ValidFrom");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Entries",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Entries",
+                table: "Entries",
+                columns: new[] { "IndicatorId", "Id", "StartDate" });
+        }
+    }
+}

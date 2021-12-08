10d7ebcdb5a49996e1dbebd7da1d15e48b2fc571
Jon.Hodgins <19355502+JonHodgins@users.noreply.github.com>
2021-12-07 17:37:47 -0700

    Store datetimes that have no time component as date column types

---
+++ src/OurCleanFuture.Data/Migrations/20211207233517_ConvertDatetimesToDates.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OurCleanFuture.Data.Migrations
+{
+    public partial class ConvertDatetimesToDates : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Targets",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Targets",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.Sql("ALTER TABLE [Entries] SET (SYSTEM_VERSIONING = OFF)");
+            migrationBuilder.Sql("ALTER TABLE [Entries] DROP CONSTRAINT [PK_Entries];");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Entries",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "EntriesHistory",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Entries",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "EntriesHistory",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.Sql("ALTER TABLE [Entries] ADD CONSTRAINT [PK_Entries] PRIMARY KEY ([IndicatorId], [StartDate]);");
+            migrationBuilder.Sql(@"DECLARE @historyTableSchema sysname = SCHEMA_NAME()
+                EXEC(N'ALTER TABLE [Entries] SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [' + @historyTableSchema + '].[EntriesHistory]))')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TargetCompletionDate",
+                table: "Actions",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Actions",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ActualCompletionDate",
+                table: "Actions",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Targets",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Targets",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.Sql("ALTER TABLE [Entries] SET (SYSTEM_VERSIONING = OFF)");
+            migrationBuilder.Sql("ALTER TABLE [Entries] DROP CONSTRAINT [PK_Entries];");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Entries",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "EntriesHistory",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Entries",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "EntriesHistory",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "date");
+
+            migrationBuilder.Sql("ALTER TABLE [Entries] ADD CONSTRAINT [PK_Entries] PRIMARY KEY ([IndicatorId], [StartDate]);");
+            migrationBuilder.Sql(@"DECLARE @historyTableSchema sysname = SCHEMA_NAME()
+                EXEC(N'ALTER TABLE [Entries] SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE = [' + @historyTableSchema + '].[EntriesHistory]))')");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "TargetCompletionDate",
+                table: "Actions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "StartDate",
+                table: "Actions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ActualCompletionDate",
+                table: "Actions",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "date",
+                oldNullable: true);
+        }
+    }
+}

06390431b84ff52229a64726ed01cd7e683cb2d6
JonHodgins <https://ourcleanfuture.ynet.gov.yk.ca/indicators/details/1>
2022-03-23 22:44:49 -0700

    Add remove/add user functions to user authorizations page

--- src/OurCleanFuture.Data/Entities/User.cs
+++ src/OurCleanFuture.Data/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,7 @@ namespace OurCleanFuture.Data.Entities;
 public class User
 {
     public int Id { get; set; }
+    [EmailAddress]
     public string Email { get; set; } = "";
     public List<Lead> Leads { get; set; } = new();
 }

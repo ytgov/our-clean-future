3c46f9f4685c448a87dee8756afd973733d49ef1
JonHodgins <https://ourcleanfuture.ynet.gov.yk.ca/indicators/details/1>
2022-03-25 11:55:57 -0700

    Convert program/startup classes to .net 6 minimal api style

--- src/OurCleanFuture.App/Program.cs
+++ src/OurCleanFuture.App/Program.cs
@@ -1,43 +1,156 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.EntityFrameworkCore;
+using MudBlazor.Services;
+using OurCleanFuture.App;
+using OurCleanFuture.Data;
 using Serilog;
 using Serilog.Events;
 
-namespace OurCleanFuture.App;
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-public static class Program
-{
-    public static int Main(string[] args)
-    {
-        Log.Logger = new LoggerConfiguration()
-        .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
-        .Enrich.FromLogContext()
-        .WriteTo.Console()
-        .CreateBootstrapLogger();
+try {
+    Log.Information("Starting web host");
+    var builder = WebApplication.CreateBuilder(args);
 
-        try {
-            Log.Information("Starting web host");
-            CreateHostBuilder(args).Build().Run();
-            return 0;
-        }
-        catch (Exception ex) {
-            Log.Fatal(ex, "Host terminated unexpectedly");
-            return 1;
-        }
-        finally {
-            Log.CloseAndFlush();
-        }
-    }
+    var configuration = builder.Configuration;
 
-    public static IHostBuilder CreateHostBuilder(string[] args)
-    {
-        return Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, services, configuration) => configuration
-                .ReadFrom.Configuration(context.Configuration)
-                .ReadFrom.Services(services)
-                .Enrich.FromLogContext())
-                .ConfigureWebHostDefaults(webBuilder => {
-                    webBuilder.UseStaticWebAssets();
-                    webBuilder.UseStartup<Startup>();
-                })
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+                    .ReadFrom.Configuration(context.Configuration)
+                    .ReadFrom.Services(services)
+                    .Enrich.FromLogContext())
                 .UseWindowsService();
+
+    // Add services to the container.
+    builder.Services.AddSingleton(configuration);
+    builder.Services.AddScoped<IClaimsTransformation, AddRoleClaimsTransformation>();
+
+    builder.Services.AddRazorPages();
+    builder.Services.AddServerSideBlazor();
+
+    builder.Services.Configure<CookiePolicyOptions>(options => {
+        options.CheckConsentNeeded = context => true;
+        options.MinimumSameSitePolicy = SameSiteMode.None;
+    });
+
+    // Add authentication services
+    builder.Services.AddAuthentication(options => {
+        options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie(options => {
+        options.LoginPath = $"/{configuration["AuthNProvider:LoginPath"]}";
+        options.LogoutPath = $"/{configuration["AuthNProvider:LogoutPath"]}";
+    })
+    .AddOpenIdConnect(configuration["AuthNProvider:Name"], options => {
+        options.Authority = $"https://{configuration["AuthNProvider:Domain"]}";
+        options.ClientId = configuration["AuthNProvider:ClientId"];
+        options.ClientSecret = configuration["AuthNProvider:ClientSecret"];
+        options.ResponseType = "code";
+
+        options.Scope.Clear();
+        options.Scope.Add("openid");
+        options.Scope.Add("profile");
+        options.Scope.Add("email");
+
+        options.CallbackPath = configuration["AuthNProvider:CallbackPath"];
+        options.SignedOutCallbackPath = configuration["AuthNProvider:SignedOutCallbackPath"];
+        options.SignedOutRedirectUri = configuration["AuthNProvider:SignedOutRedirectUri"];
+        options.ClaimsIssuer = configuration["AuthNProvider:Name"];
+
+        options.Events = new OpenIdConnectEvents {
+            OnRedirectToIdentityProviderForSignOut = (context) => {
+                var logoutUri = $"https://{configuration["AuthNProvider:Domain"]}/v2/logout?client_id={configuration["AuthNProvider:ClientId"]}";
+
+                var postLogoutUri = context.Properties.RedirectUri;
+                if (!string.IsNullOrEmpty(postLogoutUri)) {
+                    if (postLogoutUri.StartsWith("/")) {
+                        var request = context.Request;
+                        postLogoutUri = request.Scheme + "://" + request.Host + request.PathBase + postLogoutUri;
+                    }
+                    logoutUri += $"&returnTo={ Uri.EscapeDataString(postLogoutUri)}";
+                }
+
+                context.Response.Redirect(logoutUri);
+                context.HandleResponse();
+
+                return Task.CompletedTask;
+            }
+        };
+
+        options.Events.OnSignedOutCallbackRedirect = (context) => {
+
+            context.Response.Redirect(options.SignedOutRedirectUri);
+            context.HandleResponse();
+
+            return Task.CompletedTask;
+        };
+
+    });
+
+    // TODO: Adding the authz fallback policy is resulting in an infinite loop of login redirects.
+    //services.AddAuthorization(options => {
+    //    options.FallbackPolicy = new AuthorizationPolicyBuilder()
+    //        .RequireAuthenticatedUser()
+    //        .Build();
+    //});
+
+    builder.Services.AddHttpContextAccessor();
+
+    builder.Services.AddMudServices();
+#if DEBUG
+    builder.Services.AddDbContextFactory<AppDbContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("AppDbContext"), options => options.EnableRetryOnFailure())
+               .EnableSensitiveDataLogging());
+#else
+                services.AddDbContextFactory<AppDbContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("AppDbContext"), options => options.EnableRetryOnFailure()));
+#endif
+
+    builder.Services.AddLocalization();
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment()) {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
     }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+
+    app.UseRouting();
+    app.UseRequestLocalization("en-CA");
+
+    app.UseCookiePolicy();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapBlazorHub();
+    app.MapFallbackToPage("/_Host");
+
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Seq("http://localhost:5341")
+        .CreateLogger();
+
+    app.Run();
+    return 0;
 }
+catch (Exception ex) {
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    Console.WriteLine(ex.Message);
+    return 1;
+}
+finally {
+    Log.CloseAndFlush();
+}
+

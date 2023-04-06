0cce035d8b3a59c52bd1fe06b56a5858dd19ca10
JonHodgins <19355502+JonHodgins@users.noreply.github.com>
2023-04-06 16:06:46 -0700

    Fix: The identity provider is no longer providing a "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name" claim for the user principal

--- src/OurCleanFuture.App/Extensions/ClaimsPrincipalExtensions.cs
+++ src/OurCleanFuture.App/Extensions/ClaimsPrincipalExtensions.cs
@@ -4,6 +4,13 @@ namespace OurCleanFuture.App.Extensions;
 
 public static class ClaimsPrincipalExtensions
 {
-    public static string GetFormattedName(this ClaimsPrincipal claimsPrincipal) =>
-        claimsPrincipal.FindFirst(ClaimTypes.Name)?.Value.Replace('.', ' ') ?? "";
+    public static string GetFormattedName(this ClaimsPrincipal claimsPrincipal)
+    {
+        var email = claimsPrincipal.FindFirst(ClaimTypes.Email);
+        if (email == null)
+        {
+            return string.Empty;
+        }
+        return email.Value.Substring(0, email.Value.IndexOf('@')).Replace('.', ' ');
+    }
 }
